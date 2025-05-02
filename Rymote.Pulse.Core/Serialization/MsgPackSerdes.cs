8b9b6375039b589648a09e7d8f88a5c5930c8b68
Jovan Ivanovic <jovanivanovic@einutech.com>
2025-05-02 04:40:21 +0200

    Add packet prefix

--- Rymote.Pulse.Core/Serialization/MsgPackSerdes.cs
+++ Rymote.Pulse.Core/Serialization/MsgPackSerdes.cs
@@ -1,10 +1,13 @@
-﻿using MessagePack;
+﻿using System.Security.Cryptography;
+using MessagePack;
 using MessagePack.Resolvers;
 
 namespace Rymote.Pulse.Core.Serialization;
 
 public static class MsgPackSerdes
 {
+    private const int PREFIX_LENGTH = 13;
+
     private static readonly MessagePackSerializerOptions _options =
         MessagePackSerializerOptions.Standard
             .WithCompression(MessagePackCompression.None)
@@ -12,8 +15,26 @@ public static class MsgPackSerdes
                 GeneratedMessagePackResolver.Instance,
                 ContractlessStandardResolver.Instance
             ));
-    
-    public static byte[] Serialize<T>(T obj) => MessagePackSerializer.Serialize(obj, _options);
 
-    public static T Deserialize<T>(byte[] data) => MessagePackSerializer.Deserialize<T>(data, _options);
+    public static byte[] Serialize<T>(T obj)
+    {
+        byte[] data = MessagePackSerializer.Serialize(obj, _options);
+        byte[] prefix = new byte[PREFIX_LENGTH];
+        RandomNumberGenerator.Fill(prefix);
+
+        byte[] result = new byte[PREFIX_LENGTH + data.Length];
+        Buffer.BlockCopy(prefix, 0, result, 0, PREFIX_LENGTH);
+        Buffer.BlockCopy(data, 0, result, PREFIX_LENGTH, data.Length);
+        return result;
+    }
+
+    public static T Deserialize<T>(byte[] data)
+    {
+        if (data.Length <= PREFIX_LENGTH)
+            throw new ArgumentException("Frame too short", nameof(data));
+
+        byte[] payload = new byte[data.Length - PREFIX_LENGTH];
+        Buffer.BlockCopy(data, PREFIX_LENGTH, payload, 0, payload.Length);
+        return MessagePackSerializer.Deserialize<T>(payload, _options);
+    }
 }
